59e163df46f5fa2d4ac9e53803c86728122403ef
Aaron Cleveland <toscleveland@gmail.com>
2020-03-07 09:49:48 -0600

    Initial Commit

---
+++ AUITests/AUITests.swift
@@ -0,0 +1,34 @@
+//
+//  AUITests.swift
+//  AUITests
+//
+//  Created by Aaron Cleveland on 3/7/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import XCTest
+@testable import AUI
+
+class AUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
